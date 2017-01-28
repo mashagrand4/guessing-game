919f6c17d58d6ffd6610f892fbadb03073be44f4
mashagrand4 <mashagrand4@gmail.com>
2017-01-28 22:23:21 +0300

    guessing-game/feature: implementation methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max;
+        this.min;
+        this.guessNumb;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNumb = Math.round((this.max - this.min) / 2) + this.min;
+        return this.guessNumb;
     }
 
     lower() {
-
+        this.max = this.guessNumb;
     }
 
     greater() {
-
+        this.min = this.guessNumb;
     }
 }
 
